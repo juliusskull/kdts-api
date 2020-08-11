f8bfd950f37d6744f6329140caf505180fa79f4a
juliusskull <juliusskull@gmail.com>
2020-08-10 22:13:45 -0300

    cambio en los select 2

--- app/model/productosModel.js
+++ app/model/productosModel.js
@@ -11,7 +11,7 @@ var Productos = function(Productos){
     this.fchalta = Productos.fchalta;
 };
 Productos.createProductos = function (newProductos, result) {    
-        sql.query("INSERT INTO Productos set ?", newProductos, function (err, res) {
+        sql.query("INSERT INTO productos set ?", newProductos, function (err, res) {
             console.log("error: -----------");
                 if(err) {
                     console.log("error: "+ err.message, err);
@@ -24,7 +24,7 @@ Productos.createProductos = function (newProductos, result) {
             });           
 };
 Productos.getProductosById = function (ProductosId, result) {
-        sql.query("Select id from Productos where id = ? ", ProductosId, function (err, res) {             
+        sql.query("Select id from productos where id = ? ", ProductosId, function (err, res) {             
                 if(err) {
                     console.log("error: ", err);
                     result(err, null);
@@ -37,7 +37,7 @@ Productos.getProductosById = function (ProductosId, result) {
 };
 
 Productos.getAllProductos = function (result) {
-        sql.query("Select * from Productos", function (err, res) {
+        sql.query("Select * from productos", function (err, res) {
 
                 if(err) {
                     console.log("error: ", err);
@@ -51,7 +51,7 @@ Productos.getAllProductos = function (result) {
             });   
 };
 Productos.updateById = function(id, Productos, result){
-  sql.query("UPDATE Productos SET  precio = ?  WHERE id = ?", [Productos.precio, id], function (err, res) {
+  sql.query("UPDATE productos SET  precio = ?  WHERE id = ?", [Productos.precio, id], function (err, res) {
           if(err) {
               console.log("error: ", err);
                 result(null, err);
@@ -62,7 +62,7 @@ Productos.updateById = function(id, Productos, result){
             }); 
 };
 Productos.remove = function(id, result){
-     sql.query("DELETE FROM Productos WHERE id = ?", [id], function (err, res) {
+     sql.query("DELETE FROM productos WHERE id = ?", [id], function (err, res) {
 
                 if(err) {
                     console.log("error: ", err);
